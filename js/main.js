0366b6929e2b3cb19597a589a1150fe3c7be3036
Olena Kuziaieva <kusaeva@gmail.com>
2019-09-16 19:38:02 +0200

    Add mapBox token

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1Ijoib2xlbmFrODkiLCJhIjoiY2swZjU4djY1MDVuYzNncngzeTV4ZHpxZSJ9.O3KeKmC9lXa0gQ7oBGRzag',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
